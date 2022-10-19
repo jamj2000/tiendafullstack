0c2c0493bb92829b253bbcc2e135ac92b845b150
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2022-10-19 13:47:57 +0200

    API finished

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,6 +4,11 @@ import { DB_URI } from '$env/static/private';
 const dbName = 'tienda'; // Database Name
 const client = new MongoClient(DB_URI);
 
+await client.connect()
+console.log(`Conectado a BD ${DB_URI}/${dbName}`)
+export default client.db(dbName) // return database
+
+
 // async function main() {
 //     try {
 //         await client.connect();
@@ -18,6 +23,13 @@ const client = new MongoClient(DB_URI);
 
 // await main().catch(console.error);
 
-await client.connect()
-console.log(`Conectado a BD ${DB_URI}/${dbName}`)
-export default client.db(dbName) // return database
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://jose:<password>@cluster0.hhmhj.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
