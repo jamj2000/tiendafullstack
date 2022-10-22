fa04f03480d5cfb7fdc2af517e6c3f80617f7542
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2022-10-22 22:08:34 +0200

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<div class="error">
+	<h1>{$page.status}: {$page.error.message}</h1>
+	<img src="/error.webp" alt="Surprised cat" />
+</div>
+
+<style>
+	.error {
+		height: 100vh;
+		display: grid;
+		gap: var(--spacing-32);
+		place-content: center;
+		place-items: center;
+	}
+
+	img {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		object-fit: cover;
+	}
+</style>
